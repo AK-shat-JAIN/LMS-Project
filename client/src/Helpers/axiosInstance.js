84509f9c3af4e4d19cb3995e8eba7df4ea4605d4
Akshat Jain <84455686+AK-shat-JAIN@users.noreply.github.com>
2023-10-12 05:10:52 +0530

    Correction in Base URl

--- client/src/Helpers/axiosInstance.js
+++ client/src/Helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = "https://lms-project-backend.onrender.com";
+const BASE_URL = "https://lms-project-backend.onrender.com/api/v1";
 
 const axiosInstance = axios.create();
 
